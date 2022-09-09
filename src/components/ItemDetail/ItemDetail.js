8af6ff2c43cc01728abd34cea7ba955868e0305c
CamiFerraro <camiferraroo@gmail.com>
2022-09-09 00:33:49 -0300

    item detail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -5,7 +5,7 @@ const ItemDetail = ({title, imageUrl, description, price}) => {
     return (
         <div className='itemDetail'>
             <h1>Detalle {title}</h1>
-            <img src={imageUrl} alt={description} height="250px"/>
+            <img src={imageUrl} alt={description} height="250px" />
             <p>{description}</p>
             <p>{price}</p>
         </div>
